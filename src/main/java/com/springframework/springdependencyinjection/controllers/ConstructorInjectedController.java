6eac89a47079bc69d2ccbe4d5e87c63c7bc359e3
deepesh9001 <dipubkn14@gmail.com>
2021-01-23 13:55:05 +0530

    Testing using main application and using qualifiers for different greeting service

--- src/main/java/com/springframework/springdependencyinjection/controllers/ConstructorInjectedController.java
+++ src/main/java/com/springframework/springdependencyinjection/controllers/ConstructorInjectedController.java
@@ -1,12 +1,15 @@
 package com.springframework.springdependencyinjection.controllers;
 
 import com.springframework.springdependencyinjection.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
     private final GreetingService greetingService;
 
-    public ConstructorInjectedController(GreetingService greetingService) {
+    public ConstructorInjectedController(@Qualifier("constructorGreetingServiceImpl") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
